59a554084291dbce13818dfc53b7f40f73696627
a <b>
2023-10-10 23:44:21 +0800

    add nest

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Card } from "ui";
 import styles from "./page.module.css";
+import { PrismaClient } from 'database'
 
 function Gradient({
   conic,
@@ -11,6 +12,7 @@ function Gradient({
   conic?: boolean;
   className?: string;
 }): JSX.Element {
+  const client = new PrismaClient();
   return (
     <span
       className={[
